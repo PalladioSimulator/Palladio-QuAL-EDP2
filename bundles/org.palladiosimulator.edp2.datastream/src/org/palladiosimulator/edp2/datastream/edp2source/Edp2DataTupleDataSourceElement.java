8dba468ace41188eb655c56db5c29986aba573c0
Ralf Sieger <sieger@fzi.de>
2025-03-10 13:54:43 +0100

    detangled data source from persistable functionality

---
+++ bundles/org.palladiosimulator.edp2.datastream/src/org/palladiosimulator/edp2/datastream/edp2source/Edp2DataTupleDataSourceElement.java
@@ -0,0 +1,22 @@
+package org.palladiosimulator.edp2.datastream.edp2source;
+
+import org.eclipse.ui.IMemento;
+import org.eclipse.ui.IPersistableElement;
+import org.palladiosimulator.edp2.datastream.elementfactories.Edp2DataTupleDataSourceFactory;
+import org.palladiosimulator.edp2.models.ExperimentData.RawMeasurements;
+
+public class Edp2DataTupleDataSourceElement extends Edp2DataTupleDataSource implements IPersistableElement {
+    public Edp2DataTupleDataSourceElement(RawMeasurements measurements) {
+        super(measurements);
+    }
+
+    @Override
+    public void saveState(final IMemento memento) {
+        Edp2DataTupleDataSourceFactory.saveState(memento, this);
+    }
+
+    @Override
+    public String getFactoryId() {
+        return Edp2DataTupleDataSourceFactory.FACTORY_ID;
+    }
+}
